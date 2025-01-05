54874f702a4ab36b7581e9c492a7ddfc6e5ee940
lee <thejinx@naver.com>
2025-01-05 13:35:30 +0900

    h2데이터베이스 연결

---
+++ src/main/java/com/example/CarBlog_2/CarBlog2Application.java
@@ -0,0 +1,13 @@
+package com.example.CarBlog_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBlog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBlog2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarBlog_2/CarBlog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarBlog_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBlog2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
