08f036f209d969d979d42639809b09287da8fbc5
lee <thejinx@naver.com>
2025-01-11 21:57:03 +0900

    게시글 작성까지완료

--- src/main/java/com/example/CarBlog_2/Board/BoardController.java
+++ src/main/java/com/example/CarBlog_2/Board/BoardController.java
@@ -3,10 +3,7 @@ package com.example.CarBlog_2.Board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,13 +13,37 @@ import java.util.List;
 public class BoardController {
 
     private final BoardService boardService;
-    private final BoardRepository boardRepository;
 
     @GetMapping("/board/list")
     public String getList(Model model){
+        String title = "목록보기";
         List<BoardDTO> boardList = this.boardService.getList();
         model.addAttribute("boardList",boardList);
+        model.addAttribute("title",title);
         return "board_list.html";
+    }
+
+    @GetMapping("/board/detail/{id}")
+    public String boardDetail(@PathVariable("id") long id,Model model){
+
+        BoardDTO boardDTO = this.boardService.getBoard(id);
+        String title = boardDTO.getId()+"님의 게시글";
+        model.addAttribute("boardDTO",boardDTO);
+        model.addAttribute("title", title);
+        return "board_detail.html";
+    }
+
+    @GetMapping("/board/create")
+    public String boardCreate(Model model){
+        String title = "게시글 작성하기";
+        model.addAttribute("title", title);
+        return "board_create.html";
+    }
+
+    @PostMapping("/board/create")
+    public String boardCreate(@RequestParam String title, @RequestParam String content){
+        this.boardService.createBoard(title, content);
+        return "redirect:/board/list";
     }
 
 
--- src/main/java/com/example/CarBlog_2/Board/BoardService.java
+++ src/main/java/com/example/CarBlog_2/Board/BoardService.java
@@ -1,9 +1,12 @@
 package com.example.CarBlog_2.Board;
 
+import com.example.CarBlog_2.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +16,23 @@ public class BoardService {
 
     public List<BoardDTO> getList(){
         return this.boardRepository.findAll();
+    }
+
+    public BoardDTO getBoard(Long id){
+        Optional<BoardDTO> boardDTO = this.boardRepository.findById(id);
+        if(boardDTO.isPresent()){
+            return boardDTO.get();
+        }else{
+            throw new DataNotFoundException("Not Found");
+        }
+
+    }
+
+    public void createBoard(String title, String content){
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setTitle(title);
+        boardDTO.setContent(content);
+        boardDTO.setCreateTime(LocalDateTime.now());
+        this.boardRepository.save(boardDTO);
     }
 }
