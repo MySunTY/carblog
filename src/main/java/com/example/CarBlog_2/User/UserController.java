25b2fb38c98ca65c2da1f57db1882e2e9d6c15dc
lee <thejinx@naver.com>
2025-01-16 15:27:49 +0900

    회원등록 중복처리

--- src/main/java/com/example/CarBlog_2/User/UserController.java
+++ src/main/java/com/example/CarBlog_2/User/UserController.java
@@ -3,6 +3,7 @@ package com.example.CarBlog_2.User;
 import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -35,7 +36,18 @@ public class UserController {
         if(bindingResult.hasErrors()){
             return "user_register.html";
         }
-        this.userService.create(userCreateForm.getUsername(), userCreateForm.getPassword(), userCreateForm.getEmail());
+        try{
+            this.userService.create(userCreateForm.getUsername(), userCreateForm.getPassword(), userCreateForm.getEmail());
+        }catch(DataIntegrityViolationException e){
+            e.printStackTrace();
+            bindingResult.reject("jungbok","이미 등록된 사용자입니다.");
+            return "user_register";
+        }catch(Exception e){
+            e.printStackTrace();
+            bindingResult.reject("registerFail",e.getMessage());
+            return "user_register";
+        }
+
         return "redirect:/board/list";
     }
 
