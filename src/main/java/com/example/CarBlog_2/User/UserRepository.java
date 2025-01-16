82248dc09d794e5ae3d1a11941fbf5311a7d7595
lee <thejinx@naver.com>
2025-01-16 23:34:14 +0900

    로그인 로그아웃 구현

--- src/main/java/com/example/CarBlog_2/User/UserRepository.java
+++ src/main/java/com/example/CarBlog_2/User/UserRepository.java
@@ -2,6 +2,9 @@ package com.example.CarBlog_2.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserDTO, Long> {
+    Optional<UserDTO> findByUsername(String username);
 
 }
