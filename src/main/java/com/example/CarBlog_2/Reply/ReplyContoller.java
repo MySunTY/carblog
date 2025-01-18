74c94593504e6755b27b82472f96ba6c6e8d956c
lee <thejinx@naver.com>
2025-01-18 23:35:28 +0900

    프로젝트 1차완성

--- src/main/java/com/example/CarBlog_2/Reply/ReplyContoller.java
+++ src/main/java/com/example/CarBlog_2/Reply/ReplyContoller.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +31,34 @@ public class ReplyContoller {
         ReplyDTO replyDTO = this.replyService.createReply(content,boardDTO,user);
         model.addAttribute("replyDTO", replyDTO);
         return String.format("redirect:/board/detail/%s",id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/reply/modify/{id}")
+    public String modifyReply(@PathVariable("id")Long id, Model model){
+        String title = "답변 수정 페이지";
+        model.addAttribute("title", title);
+        ReplyDTO replyDTO = this.replyService.getReply(id);
+        model.addAttribute("replyDTO",replyDTO);
+
+        return "reply_modify";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/reply/modify/{id}")
+    public String modifyReply(@PathVariable("id")Long id , @RequestParam String content){
+        ReplyDTO replyDTO = this.replyService.getReply(id);
+        this.replyService.modifyReply(replyDTO, content);
+        Long num = replyDTO.getBoardDTO().getId();
+        return String.format("redirect:/board/detail/%s", num);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/reply/delete/{id}")
+    public String deleteReply(@PathVariable("id") Long id){
+        ReplyDTO replyDTO = this.replyService.getReply(id);
+        this.replyService.deleteReply(replyDTO);
+        return String.format("redirect:/board/detail/%s", replyDTO.getBoardDTO().getId());
     }
 
 }
--- src/main/java/com/example/CarBlog_2/Reply/ReplyService.java
+++ src/main/java/com/example/CarBlog_2/Reply/ReplyService.java
@@ -1,11 +1,13 @@
 package com.example.CarBlog_2.Reply;
 
 import com.example.CarBlog_2.Board.BoardDTO;
+import com.example.CarBlog_2.DataNotFoundException;
 import com.example.CarBlog_2.User.UserDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,6 +23,26 @@ public class ReplyService {
         replyDTO.setUsername(username);
         this.replyRepository.save(replyDTO);
         return replyDTO;
+
+    }
+
+    public ReplyDTO getReply(Long id){
+        Optional<ReplyDTO> replyDTO = this.replyRepository.findById(id);
+        if(replyDTO.isPresent()){
+            return replyDTO.get();
+        }else{
+            throw new DataNotFoundException("Reply Not Found");
+        }
+
+    }
+
+    public void modifyReply(ReplyDTO replyDTO , String content){
+        replyDTO.setContent(content);
+        this.replyRepository.save(replyDTO);
+    }
+
+    public void deleteReply(ReplyDTO replyDTO){
+        this.replyRepository.delete(replyDTO);
 
     }
 
